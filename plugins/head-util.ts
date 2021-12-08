a024b70bb20e910fc37395a073c373e86ca11e6d
SirawichDev <sirawit0676@gmail.com>
2021-12-08 17:25:11 +0700

    feat: add image

--- plugins/head-util.ts
+++ plugins/head-util.ts
@@ -31,7 +31,7 @@ const seoInject: Plugin = (_: Context, inject) => {
       {
         property: 'og:image',
         name: 'og:image',
-        content: data.image || 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'
+        content: data.image || '/images/Yoda_Promo3.jpeg'
       }
     ],
     link: [
